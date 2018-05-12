363cc72eca5711ad151746c7aba8945aa479b0a7
Daniel Falbel <dfalbel@gmail.com>
2018-05-12 14:30:28 -0300

    adiciona funcoes que zipam as aulas

--- R/site.R
+++ R/site.R
@@ -7,6 +7,23 @@ criar_site <- function() {
   blogdown::new_site(
     dir = "site",
     sample = FALSE,
-    template = "curso-r/template-curso"
+    theme = "curso-r/template-curso"
   )
+}
+
+#' Zipar as aulas
+#'
+#' Zipa as aulas e coloca na pasta static do site, assim elas podem ser servidas
+#' pelo site.
+#'
+#' @export
+zipar_aulas <- function() {
+  pastas <- list.dirs("aulas", recursive = FALSE)
+  purrr::walk(pastas, zipar_pasta)
+}
+
+zipar_pasta <- function(pasta) {
+  arqs <- list.files(pasta, full.names = TRUE, recursive = TRUE)
+  nome_pasta <- stringr::str_replace_all(pasta, "^.*/", "")
+  zip::zip(paste0("site/static/", nome_pasta, ".zip"), pasta)
 }
